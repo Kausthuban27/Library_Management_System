777e439ff08e6b4a014cc7f011e9b59b5857dbab
Kausthuban M <mkausthuban@ceiamerica.com>
2024-05-23 17:36:19 +0530

    Updated Code

--- LibrarySQL/Tables/Librarian.sql
+++ LibrarySQL/Tables/Librarian.sql
@@ -5,7 +5,8 @@
     [Lastname] NVARCHAR(50) NOT NULL, 
     [Username] NVARCHAR(20) NOT NULL, 
     [Email] NVARCHAR(50) NOT NULL,
+    [Password] NVARCHAR(100) NOT NULL,
     [Phone] NVARCHAR(10) NOT NULL,
-    [DateOfJoining] DATETIME2(6) NOT NULL,
+    [DateOfJoining] DATE NOT NULL,
     CONSTRAINT CK_Librarian CHECK (LEN(Phone) = 10 AND Phone NOT LIKE '%[^0-9]%')
 )
---
+++ Library_WebApp/Model/Librarian.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library_WebApp.Model
+{
+    public class Librarian
+    {
+        [StringLength(20, ErrorMessage = "Firstname cannot be more than 100")]
+        [Required(ErrorMessage = "Firstname is Mandatory")]
+        public string firstname { get; set; } = null!;
+
+        [StringLength(20, ErrorMessage = "Lastname cannot be more than 20")]
+        [Required(ErrorMessage = "Lastname is Mandatory")]
+        public string lastname { get; set; } = null!;
+
+        [StringLength(20, ErrorMessage = "Username cannot be more than 20")]
+        [Required(ErrorMessage = "Username is Mandatory")]
+        public string username { get; set; } = null!;
+
+        [StringLength(100, ErrorMessage = "Email cannot be more than 100")]
+        [Required(ErrorMessage = "Email is Mandatory")]
+        public string email { get; set; } = null!;
+
+        [StringLength(20, ErrorMessage = "Password cannot be more than 20")]
+        [Required(ErrorMessage = "Password is Mandatory")]
+        public string password { get; set; } = null!;
+
+        [Required(ErrorMessage = "Phone number is Mandatory")]
+        [RegularExpression(@"^\(?\d{3}?\)??-??\(?\d{3}?\)??-??\(?\d{4}?\)??-?$", ErrorMessage = "Phone number should be in 111-111-1111 format")]
+        [StringLength(10, ErrorMessage = "Phone number cannot be more than 10")]
+        public string phone { get; set; } = null!;
+
+        public DateOnly DateOfJoining { get; set; }
+    }
+}
