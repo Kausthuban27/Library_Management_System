3e41fd971e3dac624ad835b8d6f1e84baa3ae6bc
Kausthuban M <mkausthuban@ceiamerica.com>
2024-05-27 15:23:32 +0530

    Update Code

--- LibraryData/Program.cs
+++ LibraryData/Program.cs
@@ -1,13 +1,16 @@
+using LibraryData.Models;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices((context, services) =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddDbContext<librarydbContext>();
     })
     .Build();
 
