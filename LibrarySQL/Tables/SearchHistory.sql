82583346f2f6234c57618a24f1de77233e492a7d
Kausthuban M <mkausthuban@ceiamerica.com>
2024-06-03 15:00:13 +0530

    Updated Sql Tables and scripts

---
+++ LibrarySQL/Tables/SearchHistory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[SearchHistory]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [BookName] NVARCHAR(100) NULL, 
+    [BookAuthor] NVARCHAR(100) NULL, 
+    [BookPublisher] NVARCHAR(100) NULL,
+    [Category] NVARCHAR(100) NULL
+)
